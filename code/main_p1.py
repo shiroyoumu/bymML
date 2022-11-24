18215b28105fddaaa130bffc2656510495ba5826
shiroyoumu <iii951@163.com>
2022-11-24 14:18:49 +0800

    Revert "rebuild"
    This reverts commit aff10fa164c69bb4e95aa734ad18ca28399df1a8.

---
+++ code/main_p1.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from keras.models import Sequential
+from keras.layers import *
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+import sqlite3 as lite
+from os.path import basename
+from tcn import TCN, tcn_full_summary
+from PrototypeLayer import PrototypeLayer
+
+# 文件声明
+pathDataDB = "../data/dataset_db.db"    # 数据库文件
+
+step = 1       # 预测步长
+host = ['0001', '0021', '0070', '0143', '0354', '0372']   #
+host2 = ['6707']    # 9783、9605、2636、1046、
+
+def CollectTrainData(con, host) -> pd.DataFrame:
+    dataset = pd.DataFrame()
+    for i in host:
+        df = pd.read_sql("select Mean from datasetDB where hostname='host{}'".format(i), con)
+        dataset = dataset.append(df, ignore_index=True)
+    dataset = dataset.values.astype("float32")
+    return dataset
+
+def CollectData(dataset, step):
+    dataX, dataY = [], []
+    for i in range(len(dataset) - 1 - step):
+        dataX.append(dataset[i:(i + step), 0])
+        dataY.append(dataset[i + step, 0])
+    return np.array(dataX), np.array(dataY)
+
+if __name__ == '__main__':
+    # 定义随机种子，以便重现结果
+    np.random.seed(3)
+    # 加载数据
+    con = lite.connect(pathDataDB)
+    trainSet = CollectTrainData(con, host)
+    testSet = CollectTrainData(con, host2)
+    # 收集数据
+    trainX, trainY = CollectData(trainSet, step)
+    testX, testY = CollectData(testSet, step)
+    # 整理格式
+    trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+    testX = np.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+    # 构建网络
+    model = Sequential()
+    model.add(PrototypeLayer(filters=128,
+                   kernel_size=3,
+                   dilations=[1, 2, 4, 8, 16, 32],
+                   return_sequences=True,
+                   use_attention=True,
+                   dropout_rate=0.4,
+                   use_weight_norm=True))
+    model.add(Dense(1))
+
+    # model = Sequential()
+    # model.add(TCN(1, kernel_size=3, dilations=(1, 2, 4, 8), return_sequences=False))
+    # model.add(Dense(1))
+
+    model.compile(loss='mse', optimizer='rmsprop')
+
+    model.fit(trainX, trainY, epochs=2000, batch_size=64, verbose=2)
+    model.summary()
+    # 对测试数据的Y进行预测
+    testPre = model.predict(testX)
+    # 整理
+    trainY = np.reshape(trainY, (1, trainY.shape[0]))
+    testY = np.reshape(testY, (1, testY.shape[0]))
+    # 计算RMSE误差
+    testScore = math.sqrt(mean_squared_error(testY[0], testPre[:, 0]))
+    print('Test Score: %.2f RMSE' % (testScore))
+
+    plt.suptitle("host{} in {}".format(host2[0], basename(__file__)))
+    plt.plot(testSet)
+    empty = np.empty((step, 1))
+    empty[:, :] = np.nan
+    plt.plot(np.append(empty, testPre, axis=0))
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ code/testTCN.py
@@ -0,0 +1,101 @@
+import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+from os.path import basename
+import torch.nn as nn
+from d2l import torch as d2l
+import os
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Reshape
+from keras.layers import *
+from keras import initializers
+from keras.models import Model
+import graphviz
+from sklearn.metrics import *
+import math
+from attention import Attention
+from tcn import TCN, tcn_full_summary
+import tensorflow as tf
+
+x = np.arange(10).astype("float32").reshape([1, 10, 1]) - 4.5
+y = x
+y = Conv1D(filters=2, kernel_size=3, dilation_rate=1, padding="causal", kernel_initializer="Ones")(y)
+y = Conv1D(filters=2, kernel_size=3, dilation_rate=1, padding="causal", kernel_initializer="Ones")(y)
+z1 = y
+y = w = y + x
+y = Conv1D(filters=2, kernel_size=3, dilation_rate=2, padding="causal", kernel_initializer="Ones")(y)
+y = Conv1D(filters=2, kernel_size=3, dilation_rate=2, padding="causal", kernel_initializer="Ones")(y)
+z2 = y
+y = y + w
+y = Conv1D(filters=2, kernel_size=3, dilation_rate=4, padding="causal", kernel_initializer="Ones")(y)
+y = Conv1D(filters=2, kernel_size=3, dilation_rate=4, padding="causal", kernel_initializer="Ones")(y)
+z3 = y
+
+
+
+print(z1 + z2 + z3)
+
+z = TCN(nb_filters=2,
+        kernel_size=3,
+        dilations=[1, 2, 4],
+        kernel_initializer="Ones",
+        return_sequences=True)(x)
+
+print(z)
+
+
+# x = np.arange(10).astype("float32").reshape([1, 10, 1])
+# y = x
+# y = Conv1D(filters=1, kernel_size=3, dilation_rate=1, padding="causal", kernel_initializer="Ones")(y)
+# y = Conv1D(filters=1, kernel_size=3, dilation_rate=1, padding="causal", kernel_initializer="Ones")(y)
+#
+# print(y + x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
